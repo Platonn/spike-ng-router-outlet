d202741a083f241d9fb90ff464d0569911b85c17
Krzysztof Platis <platonn.git@gmail.com>
2018-11-23 22:12:12 +0100

    get primary segments from url, using router.parseUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, PRIMARY_OUTLET, UrlTree, UrlSegmentGroup } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spike-outlets';
+
+  constructor(router: Router) {
+    const relativeRouteTree = router.parseUrl('team/123/(user/victor//support:help)?jquery=isBad#vuejs');
+    const absoluterouteTree = router.parseUrl('/team/123/(user/victor//support:help)?jquery=isBad#reactjs');
+    console.log(relativeRouteTree.root, absoluterouteTree.root);
+  }
+
+  /**
+   * USAGE:
+   * urlTree = router.parseUrl(url);
+   * getPrimarySegments(urlTree.root)
+   **/
+  getPrimarySegments(tree: UrlSegmentGroup) {
+    const currentSegments = tree.segments.map(s => s.path);
+    let childrenSegments = [];
+    if (tree.children[PRIMARY_OUTLET]) {
+      childrenSegments = this.getPrimarySegments(tree.children.primary);
+    }
+   return currentSegments.concat(childrenSegments);
+ }
 }
